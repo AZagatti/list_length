46315b121c503e226da3d63c5573ba040e451164
azagatti <andre.zagatti1@gmail.com>
2021-03-10 22:36:48 -0300

    feat: create ListLength module and test

---
+++ lib/list_length.ex
@@ -0,0 +1,12 @@
+defmodule ListLength do
+  def call(list), do: calculate_length(list, 0)
+
+  defp calculate_length([], acc) do
+    acc
+  end
+
+  defp calculate_length([_head | tail], acc) do
+    acc = acc + 1
+    calculate_length(tail, acc)
+  end
+end
